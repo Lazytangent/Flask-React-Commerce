2ecbdde11f0edca67f6e435430af08cb5bb06ce2
Lazytangent <pm123633@gmail.com>
2021-05-29 20:27:02 -0500

    Creating forms for stocks API

--- app/forms/BuyStock.py
+++ app/forms/BuyStock.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, StringField
-from wtforms.validators import ValidationError, DataRequired
+from wtforms.validators import ValidationError, DataRequired, NumberRange
 
 
 class BuyStockForm(FlaskForm):
-    pass
+    symbol = StringField('symbol', validators=[DataRequired()])
+    shares = IntegerField('shares', validators=[DataRequired(),
+        NumberRange(min=1, message="Must provide positive number of shares")])
---
+++ app/forms/Sell.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired, NumberRange
+
+
+def enough_stock(form, field):
+
+
+
+class SellForm(FlaskForm):
