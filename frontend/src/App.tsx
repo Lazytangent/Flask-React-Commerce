b87218da537d64f5d2510761e123cc481ddf2cd7
Lazytangent <pm123633@gmail.com>
2021-05-30 17:09:38 -0500

    Create Home component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
+import { authenticate } from './store/session';
+import Home from './components/Home';
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(authenticate());
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
   );
-}
+};
 
 export default App;
