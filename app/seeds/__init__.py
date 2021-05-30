9e99d3fbd3939cdb9950dc9aa15fcecae89ce2f7
Lazytangent <pm123633@gmail.com>
2021-05-30 13:34:16 -0500

    Update seeds

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,13 +1,15 @@
 from flask.cli import AppGroup
 
+from .users import seed_users, unseed_users
+
 seed_commands = AppGroup('seed')
 
 
 @seed_commands.command('all')
 def seed():
-    pass
+    seed_users()
 
 
 @seed_commands.command('undo')
 def undo():
-    pass
+    unseed_users()
