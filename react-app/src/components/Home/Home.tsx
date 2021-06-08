b8715d86be94f863bb785c68bc39df6595a21548
Lazytangent <pm123633@gmail.com>
2021-06-08 18:48:29 -0500

    Test render of Home

--- react-app/src/components/Home/Home.tsx
+++ react-app/src/components/Home/Home.tsx
@@ -1,6 +1,21 @@
+import { useEffect } from 'react';
+
+import { Transaction } from '../../store/types.d';
+import { useAppDispatch, useAppSelector } from '../../store';
+import { getPortfolio } from '../../store/transactions';
+
 const Home = () => {
+  const dispatch = useAppDispatch();
+  const transactions = useAppSelector((state) => state.transactions.list.map((id: number) => state.transactions[id]));
+
+  useEffect(() => {
+    dispatch(getPortfolio());
+  }, [dispatch]);
+
   return (
-    <p>Home works</p>
+    <>
+      {transactions.map((transaction: Transaction) => <p>{transaction.id}</p>)}
+    </>
   );
 };
 
