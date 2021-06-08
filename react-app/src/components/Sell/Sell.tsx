d352588096feb43c9fe7c963c0f2f2c05199aa8a
Lazytangent <pm123633@gmail.com>
2021-06-08 18:42:09 -0500

    Sell component logic

--- react-app/src/components/Sell/Sell.tsx
+++ react-app/src/components/Sell/Sell.tsx
@@ -1,6 +1,37 @@
+import { useState } from 'react';
+
+import { useAppDispatch } from '../../store';
+import { sellStock } from '../../store/transactions';
+
 const Sell = () => {
+  const dispatch = useAppDispatch();
+
+  const [symbol_, setSymbol_] = useState('');
+  const [shares, setShares] = useState(0);
+
+  const submitHandler = (e: React.ChangeEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    dispatch(sellStock({ symbol: symbol_, shares }));
+  };
+
   return (
-    <p>Sell works</p>
+    <>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          value={symbol_}
+          onChange={(e) => setSymbol_(e.target.value)}
+          placeholder="Symbol"
+        />
+        <input
+          type="number"
+          value={shares}
+          onChange={(e) => setShares(parseInt(e.target.value))}
+          placeholder="Shares"
+        />
+        <button type="submit">Sell Stock</button>
+      </form>
+    </>
   );
 };
 
